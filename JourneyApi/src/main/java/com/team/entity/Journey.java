bd0353515859924d62bb30067e0c051a68f8e1c9
Abdul Rizvi <kh_abdul@hotmail.co.uk>
2022-03-15 18:55:36 +0000

    incomplete logic to journeyApi

--- JourneyApi/src/main/java/com/team/entity/Journey.java
+++ JourneyApi/src/main/java/com/team/entity/Journey.java
@@ -2,6 +2,7 @@ package com.team.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -20,22 +21,23 @@ import lombok.ToString;
 @Entity
 public class Journey {
 
-	public Journey(int id, int userId, int startStationId, LocalDateTime startTime, double price) {
-		this.id = id;
-		this.userId = userId;
-		this.startStationId = startStationId;
-		this.startTime = startTime;
-		this.price = price;
-	}
+//	public Journey(int id, int userId, int startStationId, LocalDateTime startTime, double price) {
+//		this.id = id;
+//		this.userId = userId;
+//		this.startStationId = startStationId;
+//		this.startTime = startTime;
+//		this.price = price;
+//	}
+	
+//	@Getter
+//	@Setter
+//	@Id
+//	private int id;
+	
 	
 	@Getter
 	@Setter
 	@Id
-	private int id;
-	
-	
-	@Getter
-	@Setter
 	private int userId;
 	
 	@Getter
@@ -44,21 +46,40 @@ public class Journey {
 	
 	@Getter
 	@Setter
+	@Column(nullable = true)
 	private int endStationId;
 	
 	@Getter
 	@Setter
+	@Column(nullable = true)
 	private LocalDateTime startTime;
 	
 	@Getter
 	@Setter
+	@Column(nullable = true)
 	private LocalDateTime endTime;
 	
 	@Getter
 	@Setter
+	@Column(nullable = true)
 	private double price;
 	
 	@Getter
 	@Setter
 	private boolean applyFine;
+
+	public Journey(int userId, int startStationId, LocalDateTime startTime) {
+		super();
+		this.userId = userId;
+		this.startStationId = startStationId;
+		this.startTime = startTime;
+	}
+
+	public Journey(int userId, int startStationId) {
+		super();
+		this.userId = userId;
+		this.startStationId = startStationId;
+	}
+	
+	
 }
