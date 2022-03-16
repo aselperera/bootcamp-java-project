61c1e99ad13cfbb0b04852b8c82592ef7707773c
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2022-03-16 10:07:37 +0000

    Swipe in and swipe out pages now connected

--- MetroWebApp/src/main/java/com/team/controllers/StationController.java
+++ MetroWebApp/src/main/java/com/team/controllers/StationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,14 +28,18 @@ public class StationController {
 		ModelAndView modelAndView = new ModelAndView();
 		List<Station> allStations = stationService.getAllStations();
 		modelAndView.addObject("stations", allStations);
+		modelAndView.addObject("station", new Station());
 		modelAndView.setViewName("swipeIn");
 		return modelAndView;
 	}
 	
 	@RequestMapping("/swipeOutForm")
-	public ModelAndView getSwipeOut() {
+	public ModelAndView getSwipeOut(@ModelAttribute("station") Station station) {
 		ModelAndView modelAndView = new ModelAndView();
+		Station stat = stationService.getStationById(station.getSequenceNumber());
+		String message = "You have successfully swiped in at " + stat.getStationName();
 		List<Station> allStations = stationService.getAllStations();
+		modelAndView.addObject("message", message);
 		modelAndView.addObject("stations", allStations);
 		modelAndView.setViewName("swipeOut");
 		return modelAndView;
